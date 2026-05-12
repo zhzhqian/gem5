1b4fc1803a5439b7c94c1563540df4734347e542
zq414277 <zq414277@alibaba-inc.com>
2026-05-12 10:21:30 +0800

    TaskID:12345678: make starter_fs.py run realview_v2
    Root Cause: add your root cause
    Solution: add your solution
    Test Suggestion: add your suggestion

--- configs/example/arm/starter_fs.py
+++ configs/example/arm/starter_fs.py
@@ -103,6 +103,7 @@ def create(args):
     system = devices.SimpleSystem(
         want_caches,
         args.mem_size,
+        platform=VExpress_GEM5_V2(),
         mem_mode=mem_mode,
         workload=ArmFsLinux(object_file=SysPaths.binary(args.kernel)),
         readfile=args.script,
